92f1faeda0b6c36719eeb5fa04ca47943a4b2183
LAWRENCE NICASTRO <lawrence.nicastro1@gmail.com>
2020-10-22 13:14:40 -0400

    add config with api key and api url

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
+      const api = `http://api.weatherapi.com/v1/forecast.json?key=${config.apiKey}&q=${lat},${long}&days=1`;
+      fetch(api)
+        .then((response) => {
+          return response;
+        })
+        .then((data) => console.log(data));
     });
   } else {
     window.alert("Error, please enable location services on your browser");
